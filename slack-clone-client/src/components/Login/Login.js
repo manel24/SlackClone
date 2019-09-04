aefe7393c3b8aba99ca6ed4e9885f999e51ef4ad
manelcharouni <manelcharouni@think-it.io>
2019-09-04 15:46:44 +0100

    added login form using mobx and login mutation

---
+++ slack-clone-client/src/components/Login/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { graphql } from '@apollo/react-hoc';
+import { gql } from 'apollo-boost';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Button, Container, Header, Form, Input } from 'semantic-ui-react';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        extendObservable(this, {
+            email: '',
+            password: ''
+        });
+    }
+    onChangeHandler = e => {
+        const { name, value } = e.target;
+        this[name] = value;
+    }
+    onSubmitHandler = async () => {
+        const { email, password } = this;
+        const response = await this.props.mutate({ variables: { email, password } })
+        const { ok, errors } = response.data.login;
+        if (ok) {
+            console.log('user logged in successfully!');
+            this.props.history.push('/')
+        }
+        if (errors) {
+            // const err = {};
+            // errors.forEach(({ path, message }) => {
+            //     err[`${path}Error`] = message;
+            //     this.setState(err);
+
+            // }
+            // )
+            console.log(errors)
+        }
+        console.log(response);
+
+    }
+    render() {
+        const { email, password } = this;
+        return (<Container text>
+            <Header as="h2">Login</Header>
+
+            <Form onSubmit={this.onSubmitHandler}>
+
+                <Form.Field>
+                    <label>Email</label>
+                    <Input name="email" placeholder='Email' fluid value={email} onChange={this.onChangeHandler} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <Input name="password" type="password" placeholder='*********' fluid value={password} onChange={this.onChangeHandler} autoComplete="current-password" />
+                </Form.Field>
+
+                <Button type='submit' >Submit</Button>
+            </Form>
+        </Container>)
+    }
+    I
+};
+
+const loginMutation = gql`
+mutation( $email:String!, $password:String!)
+{login(email:$email, password:$password){ok errors {path message } token refreshToken}}
+`;
+export default graphql(loginMutation)(observer(Login));
--- slack-clone-client/src/routes/index.js
+++ slack-clone-client/src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from '../components/Home/Home';
 import Register from '../components/Register/Register';
+import Login from '../components/Login/Login';
 import {
     BrowserRouter,
     Route,
@@ -11,6 +12,7 @@ export default () => (<BrowserRouter>
     <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/register" exact component={Register} />
+        <Route path="/login" exact component={Login} />
     </Switch>
 </BrowserRouter>)
 
